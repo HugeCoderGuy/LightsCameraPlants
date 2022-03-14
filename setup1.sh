e642246cee093f8ac0cbd12e30396af3c700535d
HugeCoderGuy <alexscottlewis@gmail.com>
2022-03-13 18:59:02 -0700

    "experimenting with automated setup files"

--- setup1.sh
+++ setup1.sh
@@ -5,6 +5,17 @@ sudo pip install --upgrade setuptools
 sudo apt full-upgrade
 sudo apt clean
 cd ~
+# install packages
+sudo pip install numpy
+sudo pip install
+sudo apt update
+sudo apt install python3-opencv
+sudo pip install plantcv
+sudo pip install board
+sudo pip install imutils
+sudo pip install Pillow
+pip install PyDrive
+# install adafruit packages
 sudo pip install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 sudo python raspi-blinka.py
